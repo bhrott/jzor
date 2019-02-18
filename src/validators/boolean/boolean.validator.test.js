ce5c5275c74b4bf6b8c3204ee9bd22d44881a3b4
Sergio David Fernández <mr.sdf88@gmail.com>
2019-02-18 13:31:31 -0300

    feat(boolean): adding strict mode to boolean validator

--- src/validators/boolean/boolean.validator.test.js
+++ src/validators/boolean/boolean.validator.test.js
@@ -12,7 +12,7 @@ test('invalid type should return errors', () => {
   validator.validate(ctx)
 
   expect(ctx.errors).toEqual({
-    '$root': {
+    $root: {
       type: {
         message: 'The value should be a boolean',
         value: ctx.value
@@ -31,5 +31,79 @@ test('valid type should return no errors', () => {
 
   validator.validate(ctx)
 
+  expect(ctx.valid).toBeTruthy()
+})
+
+test('no strict mode with valid boolean like FALSE should return no errors', () => {
+  let ctx
+
+  // 'false'
+  ctx = createContext({
+    schema: {
+      $type: 'boolean',
+      strict: false
+    },
+    value: 'false'
+  })
+  validator.validate(ctx)
+  expect(ctx.valid).toBeTruthy()
+
+  // '0'
+  ctx = createContext({
+    schema: {
+      $type: 'boolean',
+      strict: false
+    },
+    value: '0'
+  })
+  validator.validate(ctx)
+  expect(ctx.valid).toBeTruthy()
+
+  // 0
+  ctx = createContext({
+    schema: {
+      $type: 'boolean',
+      strict: false
+    },
+    value: 0
+  })
+  validator.validate(ctx)
+  expect(ctx.valid).toBeTruthy()
+})
+
+test('no strict mode with valid boolean like TRUE should return no errors', () => {
+  let ctx
+
+  // 'true'
+  ctx = createContext({
+    schema: {
+      $type: 'boolean',
+      strict: false
+    },
+    value: 'true'
+  })
+  validator.validate(ctx)
+  expect(ctx.valid).toBeTruthy()
+
+  // '1'
+  ctx = createContext({
+    schema: {
+      $type: 'boolean',
+      strict: false
+    },
+    value: '1'
+  })
+  validator.validate(ctx)
+  expect(ctx.valid).toBeTruthy()
+
+  // 1
+  ctx = createContext({
+    schema: {
+      $type: 'boolean',
+      strict: false
+    },
+    value: 1
+  })
+  validator.validate(ctx)
   expect(ctx.valid).toBeTruthy()
 })
